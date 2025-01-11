2c1493bcde0a25c8b39c44ec47f8243d8ffce5b1
Razvan <razvanandreibalauru@gmail.com>
2025-01-11 12:36:37 +0200

    vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 import image from '@astrojs/image';
-
+import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
   integrations: [image()],
+  output: 'server',
+  adapter: vercel(),
 });
